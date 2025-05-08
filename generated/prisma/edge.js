b5f1b0836cb775a0277bfc9d963072572a63d6e6
Julio Cesar Andrade da Rosa Junior <juliojunior@Mac-mini-de-Julio.local>
2025-05-08 19:48:23 -0300

    fix: change prisma service initialize module

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -164,11 +164,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgres@localhost:5432/cephubdb?schema=public"
+        "value": null
       }
     }
   },
